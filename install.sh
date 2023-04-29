59164602e091bc0e7a8d37bd207dd170d5b6dbad
Lanky Lonky <105496277+lankylonky22@users.noreply.github.com>
2023-04-29 18:16:19 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -37,7 +37,7 @@ os_version=""
 
 # os version
 if [[ -f /etc/os-release ]]; then
-    os_version=$(awk -F'[= ."]' '/VERSION_ID/{print $3}' /etc/os-release)
+    os_version=$(awk -F'[= ."]' '/BUILD_ID/{print $2}' /etc/os-release)
 
 
 install_base() {
